e4211573f4dc024f184dd32db1c9cc2b94f37686
Manar <manar.salah.691@gmail.com>
2022-08-30 06:22:26 +0200

    edit timer driver

---
+++ Src/Mcal/Timer.c
@@ -0,0 +1,129 @@
+/**********************************************************************************************************************
+ *  FILE DESCRIPTION
+ *  -----------------------------------------------------------------------------------------------------------------*/
+/**        \file  Timer.c
+ *        \brief  Source file for Timer module.
+ *
+ *      \details  
+ *
+ *
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  INCLUDES
+ *********************************************************************************************************************/
+#include "Timer.h"
+
+/**********************************************************************************************************************
+*  LOCAL MACROS CONSTANT\FUNCTION
+**********************************************************************************************************************/
+
+/* SysTick Registers. */
+#define SYSTICK_CTRL_REG          (*((volatile uint32 *)0xE000E010))
+#define SYSTICK_RELOAD_REG        (*((volatile uint32 *)0xE000E014))
+#define SYSTICK_CURRENT_REG       (*((volatile uint32 *)0xE000E018))
+	
+/**********************************************************************************************************************
+ *  LOCAL DATA 
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  GLOBAL DATA
+ *********************************************************************************************************************/
+ 
+static  void (*Systick_Call_Back_Ptr)(void) = NULL_PTR;
+
+/**********************************************************************************************************************
+ *  LOCAL FUNCTION PROTOTYPES
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  LOCAL FUNCTIONS
+ *********************************************************************************************************************/
+
+/**********************************************************************************************************************
+ *  GLOBAL FUNCTIONS
+ *********************************************************************************************************************/
+
+
+/******************************************************************************
+* \Syntax          : void Systick_start(Timer_ValueType tick_time)        
+* \Description     : Enable and set SysTick timer.                                    
+*                                                                             
+* \Sync\Async      : Asynchronous                                               
+* \Reentrancy      : Reentrant                                             
+* \Parameters (in) : tick_time - tick time in millisecond.                     
+* \Parameters (out): None                                                      
+* \Return value:   : None.                                  
+*******************************************************************************/
+void Systick_start(Systick_ValueType tick_time)
+{
+	/* Disable SysTick timer by clearing ENABLE bit in the CTRL register. */
+	SYSTICK_CTRL_REG = 0;
+	/* To count tick time by second. */
+	SYSTICK_RELOAD_REG = 15999999 * tick_time;
+	
+	SYSTICK_CURRENT_REG = 0;
+	/* Use system clock by setting CLK_SRC bit in the CTRL register.
+	 * Enable SysTick interrupt by setting INTEN bit in the CTRL register.
+	 * Enable SysTick timer by setting ENABLE bit in the CTRL register. */
+	SYSTICK_CTRL_REG = 0x07;
+}
+
+
+/******************************************************************************
+* \Syntax          : void Systick_stop(void)        
+* \Description     : Disable SysTick timer.                                    
+*                                                                             
+* \Sync\Async      : Synchronous                                               
+* \Reentrancy      : Reentrant                                             
+* \Parameters (in) : None.                     
+* \Parameters (out): None.                                                     
+* \Return value:   : None.                                  
+*******************************************************************************/
+void Systick_stop(void)
+{
+	/* Disable SysTick timer by clearing ENABLE bit in the CTRL register. */
+	SYSTICK_CTRL_REG = 0;
+}
+
+
+/******************************************************************************
+* \Syntax          : void Systick_setCallBack(void)        
+* \Description     : Function to set which function will call it back.                                    
+*                                                                             
+* \Sync\Async      : Synchronous                                               
+* \Reentrancy      : Reentrant                                             
+* \Parameters (in) : None                      
+* \Parameters (out): None.                                                      
+* \Return value:   : None.                                  
+*******************************************************************************/
+void Systick_setCallBack(void)
+{
+	Systick_Call_Back_Ptr = SysTick_CallBackFun();
+}
+
+
+/******************************************************************************
+* \Syntax          : void SysTick_Handler(void)       
+* \Description     : SysTick timer ISR.                                     
+*                                                                             
+* \Sync\Async      : Synchronous                                               
+* \Reentrancy      : Reentrant                                             
+* \Parameters (in) : None.                      
+* \Parameters (out): None.                                                      
+* \Return value:   : None.                                  
+*******************************************************************************/
+void SysTick_Handler(void)
+{
+	/* If Systick_setCallBack clled the value of Systick_Call_Back_Ptr will change to the call back funtion. */
+	if(Systick_Call_Back_Ptr != NULL_PTR)
+	{
+		(*Systick_Call_Back_Ptr)();
+	}
+}
+
+
+/**********************************************************************************************************************
+ *  END OF FILE: Timer.c
+ *********************************************************************************************************************/
