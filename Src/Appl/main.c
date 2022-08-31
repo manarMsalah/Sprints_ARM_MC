a120fb54b3aeae85ea8720c277b3af1878d8cd51
Manar <manar.salah.691@gmail.com>
2022-08-31 10:23:19 +0200

    edit application

--- Src/Appl/main.c
+++ Src/Appl/main.c
@@ -1,10 +1,19 @@
 
-#include "Dio.h"
 #include "Port.h"
+#include "IntCtrl.h"
+#include "Timer.h"
+#include "Led.h"
 
 
 int main (void)
 {
+	Port_Init(&Port_Configuration);
+	IntCtrl_init(&IntCtrl_Configuration);
+	
+	Systick_setCallBack(Led_toggel);
+	Systick_start(1);
+	
+
 	while(1)
 	{
 		
