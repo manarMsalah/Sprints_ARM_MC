4b2d5595d8d305ef7f9e72d5d108e2905520e8ef
Manar <manar.salah.691@gmail.com>
2022-08-31 11:01:31 +0200

    edit

--- Src/Appl/main.c
+++ Src/Appl/main.c
@@ -4,8 +4,8 @@
 #include "Timer.h"
 #include "Led.h"
 
-#define Enable_Global_Interrupts()    __asm("CPSIE I")
-#define Disable_Global_Interrupts()   __asm("CPSID I")
+#define Enable_Global_Interrupt()    __asm("CPSIE I")
+#define Disable_Global_Interrupt()   __asm("CPSID I")
 
 
 #define NUMBER_OF_SECONDS      1
@@ -17,7 +17,7 @@ int main (void)
 	IntCtrl_init(&IntCtrl_Configuration);
 	
 	Systick_setCallBack(Led_toggel);
-	Enable_Global_Interrupts();
+	Enable_Global_Interrupt();
 	Systick_start(NUMBER_OF_SECONDS);
 	
 
